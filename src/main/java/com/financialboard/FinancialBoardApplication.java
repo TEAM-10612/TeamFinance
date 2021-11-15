3e3c8331e7a1c3e0181f80c56e9121283e48dcc0
ryudongjae <rdj10149@gmail.com>
2021-11-15 16:35:43 +0900

    Initial commit

---
+++ src/main/java/com/financialboard/FinancialBoardApplication.java
@@ -0,0 +1,13 @@
+package com.financialboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financialboard/FinancialBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
