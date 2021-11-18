e0fd020916c145b7ff41e7e32f830b097903798f
ryudongjae <rdj10149@gmail.com>
2021-11-18 14:51:22 +0900

    로그인 기능 구현

--- src/main/java/com/financialboard/annotation/CurrentUser.java
+++ src/main/java/com/financialboard/annotation/CurrentUser.java
@@ -8,12 +8,4 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface CurrentUser {
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> Revert "Revert "로그인 기능 구현""
-=======
-}
->>>>>>> origin/develop
