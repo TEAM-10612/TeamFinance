46a47c79384ae14fb54f36de53f07e90e963167b
ryudongjae <rdj10149@gmail.com>
2021-11-21 18:17:10 +0900

    좋아요 기능 구현 _1

--- src/main/java/com/financialboard/repository/LikesRepository.java
+++ src/main/java/com/financialboard/repository/LikesRepository.java
@@ -1,7 +1,12 @@
 package com.financialboard.repository;
 
 import com.financialboard.model.likes.Likes;
+import com.financialboard.model.post.Post;
+import com.financialboard.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikesRepository  {
+import java.util.Optional;
+
+public interface LikesRepository extends JpaRepository<Likes,Long>{
+    Optional<Likes> findByUserAndPost(User user, Post post);
 }
