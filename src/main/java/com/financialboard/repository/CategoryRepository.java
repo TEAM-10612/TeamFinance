559ca0b094a4c26b2a6ef8a7c79cdf388c07328f
ryudongjae <rdj10149@gmail.com>
2021-11-23 20:08:53 +0900

    카테고리 서비스

--- src/main/java/com/financialboard/repository/CategoryRepository.java
+++ src/main/java/com/financialboard/repository/CategoryRepository.java
@@ -4,9 +4,10 @@ import com.financialboard.model.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     Optional<Category> findByName(String name);
     Optional<Category> findByBranchAndCode(String branch,String name);
