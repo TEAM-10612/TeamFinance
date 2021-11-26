bf0791218ba5dc36142daddf196813e7c2c7d0cf
ryudongjae <rdj10149@gmail.com>
2021-11-26 18:22:42 +0900

    게시글 API 컨트롤러

--- src/main/java/com/financialboard/controller/PostApiController.java
+++ src/main/java/com/financialboard/controller/PostApiController.java
@@ -1,14 +1,21 @@
 package com.financialboard.controller;
 
 import com.financialboard.dto.PostDto;
+import com.financialboard.exception.user.PostNotFoundException;
+import com.financialboard.model.post.Post;
 import com.financialboard.repository.PostRepository;
 import com.financialboard.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Nullable;
 import javax.validation.Valid;
+import java.util.Optional;
+
+import static com.financialboard.util.ResponseConstants.OK;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,10 +35,25 @@ public class PostApiController {
         return ResponseEntity.ok(request);
     }
 
-    @GetMapping
-    public ResponseEntity<PostDto.PostInfoResponse> getPost(@RequestBody Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto.PostInfoResponse> getPost(@PathVariable Long id){
         PostDto.PostInfoResponse postInfo = postService.getPostInfo(id);
 
         return ResponseEntity.ok(postInfo);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deletePost(@PathVariable Long id){
+        postService.deletePost(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostDto.SaveRequest> updatePost(@PathVariable Long id,
+                                                          @Valid@RequestBody PostDto.SaveRequest request
+            ,@Valid @Nullable @RequestBody MultipartFile multipartFile){
+        updatePost(id,request,multipartFile);
+
+        return ResponseEntity.ok(request);
     }
 }
