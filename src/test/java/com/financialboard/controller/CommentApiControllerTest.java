72427dfe847d4a351435917744c549a1ea9cf6f4
ryudongjae <rdj10149@gmail.com>
2022-01-14 21:04:53 +0900

    RestDocs 설정

--- src/test/java/com/financialboard/controller/CommentApiControllerTest.java
+++ src/test/java/com/financialboard/controller/CommentApiControllerTest.java
@@ -1,7 +1,27 @@
 package com.financialboard.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CommentApiControllerTest {
 
+@WebMvcTest(CommentApiController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class CommentApiControllerTest {
+    @Test
+    @DisplayName("")
+    void sadsad()throws Exception{
+    //given
+
+    //when
+
+    //then
+
+
+}
 }
